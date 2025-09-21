166b35c677ddfd4ece5ff048854728a4e23f0b8d
hammadshaikh-22 <hammadshaikh1h23@gmail.com>
2025-09-21 03:52:01 -0700

    i

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var max = 0
+var min=100000000
 var arr = [34, 54, 4, 67, 34, 90, 33, 12, 5]
 for (var i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
         max = arr[i]
     }
+    if (arr[i]<min){
+        min=arr[i]
+    }
 }
 console.log("The Largest number is: ", max)
+console.log("The smallest number is: ", min)
